6823034b540b6cbac8cd34aef5c5831d0f9a1cd2
Simon Dean <simon@simondean.org>
2011-05-12 22:32:12 +0100

    escaped the source and destination

--- spec/robocopy_spec.rb
+++ spec/robocopy_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Robocopy", "#mirror" do
   it "should mirror a directory" do
     Robocopy.stub! :sh
-    Robocopy.should_receive(:sh).with "robocopy /MIR test-source test-destination"
+    Robocopy.should_receive(:sh).with 'robocopy /MIR "test-source" "test-destination"'
     Robocopy.mirror "test-source", "test-destination"
   end
 end
