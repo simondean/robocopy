34d345148c385b9286a8e86e95c6343b425fb050
Simon Dean <simon@simondean.org>
2011-05-14 17:43:08 +0100

    trying a yield approach to options

--- lib/robocopy.rb
+++ lib/robocopy.rb
@@ -1,5 +1,20 @@
 class Robocopy
   def self.mirror source, destination
-    sh "robocopy /MIR \"#{source}\" \"#{destination}\""
+    options = Options.new
+    yield options
+
+    command = "robocopy \"#{source}\" \"#{destination}\" /MIR"
+    command += options.to_s
+    sh command
+  end
+
+  class Options
+    attr_accessor :exclude_files
+
+    def to_s
+      str = ""
+      str += " /XF \"#{@exclude_files}\"" if @exclude_files
+      str
+    end
   end
 end
--- spec/robocopy_spec.rb
+++ spec/robocopy_spec.rb
@@ -3,7 +3,16 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Robocopy", "#mirror" do
   it "should mirror a directory" do
     Robocopy.stub! :sh
-    Robocopy.should_receive(:sh).with 'robocopy /MIR "test-source" "test-destination"'
-    Robocopy.mirror "test-source", "test-destination"
+    Robocopy.should_receive(:sh).with 'robocopy "test-source" "test-destination" /MIR'
+    Robocopy.mirror "test-source", "test-destination" do |opt|
+    end
+  end
+
+  it "should accept configuration options" do
+    Robocopy.stub! :sh
+    Robocopy.should_receive(:sh).with 'robocopy "test-source" "test-destination" /MIR /XF "test-file"'
+    Robocopy.mirror "test-source", "test-destination" do |opt|
+      opt.exclude_files = 'test-file'
+    end
   end
 end
