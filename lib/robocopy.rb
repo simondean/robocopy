df98e4eb4f4ca1939aa6d6854cbdba53dc1ab926
Simon Dean <simon@simondean.org>
2011-05-14 18:54:51 +0100

    checking for unknown options

--- lib/robocopy.rb
+++ lib/robocopy.rb
@@ -1,17 +1,22 @@
 class Robocopy
   def self.mirror source, destination, options = {}
+    option_names = [:exclude_files]
+    options.each_key do |key|
+      raise ArgumentError.new "#{key} is an unknown option" unless option_names.include?(key)
+    end
+
     command = "robocopy \"#{source}\" \"#{destination}\" /MIR"
-    command += get_options_string(options)
-    sh command
+    command += get_optional_arguments(options)
+    system command
   end
 
   private
-  def unknown_options? options
+  def check_options options
     options.each_key do |key|
     end
   end
 
-  def self.get_options_string options
+  def self.get_optional_arguments options
     str = ""
     str += " /XF \"#{options[:exclude_files]}\"" if options[:exclude_files]
     str
--- spec/robocopy_spec.rb
+++ spec/robocopy_spec.rb
@@ -2,14 +2,18 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Robocopy", "#mirror" do
   it "should mirror a directory" do
-    Robocopy.stub! :sh
-    Robocopy.should_receive(:sh).with 'robocopy "test-source" "test-destination" /MIR'
+    Robocopy.stub! :system
+    Robocopy.should_receive(:system).with 'robocopy "test-source" "test-destination" /MIR'
     Robocopy.mirror "test-source", "test-destination"
   end
 
   it "should exclude files" do
-    Robocopy.stub! :sh
-    Robocopy.should_receive(:sh).with 'robocopy "test-source" "test-destination" /MIR /XF "test-file"'
+    Robocopy.stub! :system
+    Robocopy.should_receive(:system).with 'robocopy "test-source" "test-destination" /MIR /XF "test-file"'
     Robocopy.mirror "test-source", "test-destination", :exclude_files => 'test-file'
+  end
+
+  it "should reject unknown options" do
+      lambda {Robocopy.mirror "test-source", "test-destination", :test_option => 'test-value'}.should raise_error ArgumentError, "test_option is an unknown option"
   end
 end
